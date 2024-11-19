d3b359a44932f977d445f9f0eb1550b1d2dc2d50
Edt12 <104518243+Edt12@users.noreply.github.com>
2024-11-19 14:25:48 +0000

    Added Enemy.java abstract class

---
+++ src/Enemy.java
@@ -0,0 +1,34 @@
+/**
+ * The Enemy Class which all enemies inherit from
+ * @version 1.0
+ * @author Edward Tickle
+ */
+public abstract class Enemy implements MoveTo {
+    private int [] position;
+
+    @Override
+    public abstract  int[] moveTo();
+
+
+    /**
+     * Performs any actions done when an enemy dies by a hazard and returns what they should drop on their death
+     * @return int representing a particular item or set of items to be dropped on enemy death
+     */
+    public abstract int onDeathByHazard();
+
+    /**
+     * Performs any actions done when an enemy dies by a hazard and returns what they should drop on their death
+     * @return int representing a particular item or set of items to be dropped on enemy death
+     */
+    public abstract int onDeathByFallingObject();
+
+    /**
+     * Getter for Position
+     * @return Int[] Position
+     */
+    public int[] getPosition() {
+        return position;
+    }
+
+
+}
