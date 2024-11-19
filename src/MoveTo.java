15efd4fe15fc887eb7dafe13add15af1dce0ce69
Edt12 <104518243+Edt12@users.noreply.github.com>
2024-11-19 14:30:12 +0000

    Added Comment to MoveTo.java Interface

--- src/MoveTo.java
+++ src/MoveTo.java
@@ -1,4 +1,14 @@
+/**
+ * The interface to allow the player and the enemies to tell the game class where they are moving
+ * Version 1.0
+ * @author Edward Tickle
+ */
 public interface MoveTo {
+
+    /**
+     * Calculates movement of the object it is attached to
+     * @return int[] representing the coordinates to move the object to
+     */
     public int[] moveTo();
 
 }
