72cd114283894f44b44f06b004669395d7a993d9
michaelgonese <michaelgonese@outlook.com>
2024-11-21 16:49:15 +0000

    Made changes to LockDoor and implemented Key using UML

--- src/Key.java
+++ src/Key.java
@@ -1,6 +1,37 @@
 /**
- * @author Joseph Vinson
+ * @author Tafara Gonese
  */
 
 public class Key {
+    private Colour colour;
+    private int x;
+    private int y;
+
+    public Key(Colour colour, int x, int y) {
+        this.colour = colour;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Colour getColour() {
+        return colour;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean canUnlock(LockedDoor lockedDoor) {
+        return lockedDoor.getColour() == this.colour;
+    }
+
+    public void use(LockedDoor lockedDoor) {
+        if (canUnlock(lockedDoor)) {
+            lockedDoor.unlock();
+        }
+    }
 }
--- src/LockedDoor.java
+++ src/LockedDoor.java
@@ -1,12 +1,45 @@
 /**
- * @author Joseph Vinson
+ * @author Joseph Vinson, Tafara Gonese
  */
 
 public class LockedDoor extends Wall {
+    private Colour colour;
+    private boolean locked;
 
     public static final WallType wType = WallType.LOCKED_DOOR;
 
-    public LockedDoor(int x, int y) {
+    private int x;
+    private int y;
+
+    public LockedDoor(int x, int y, Colour color) {
         super(x, y, wType);
+        this.x = x;
+        this.y = y;
+        this.colour = colour;
+        this.locked = true;
+    }
+
+    public boolean canUnlock(Key key) {
+        return key.getColour() == this.colour;
+    }
+
+    public void unlock() {
+        this.locked = false;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Colour getColour() {
+        return colour;
+    }
+
+    public boolean isLocked() {
+        return locked;
     }
 }
