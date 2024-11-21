b2c4fd46978d974c5f68e0c0b27ce27b8ba486f4
JoeV05 <josephvinson183@gmail.com>
2024-11-21 12:49:47 +0000

    Updated Amoeba constructor

--- src/Amoeba.java
+++ src/Amoeba.java
@@ -3,12 +3,14 @@
  */
 
 public class Amoeba extends Tile {
+    private int size;
 
     public static final TileType TTYPE = TileType.AMOEBA;
     public static final boolean WALK = false;
+    public final int MAXIMUM_SIZE;
 
-    public Amoeba(int x, int y) {
+    public Amoeba(int x, int y, int maxSize) {
         super(x, y, WALK, TTYPE);
-
+        MAXIMUM_SIZE = maxSize;
     }
 }
