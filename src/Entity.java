afd181de1f40c6026bd98bb845adcb485d61ebdb
JoeV05 <josephvinson183@gmail.com>
2024-11-25 21:08:14 +0000

    Updated entity to include a constructor and getters/setters

--- src/Entity.java
+++ src/Entity.java
@@ -1,4 +1,29 @@
+/**
+ * @author James Harvey, Joseph Vinson
+ */
+
 public abstract class Entity {
-    protected int Xcoord;
-    protected int Ycoord;
+    protected int x;
+    protected int y;
+
+    public Entity(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
