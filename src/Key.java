4af16a05a8e6f33172ace902b5ea1fd6d582e868
JoeV05 <josephvinson183@gmail.com>
2024-11-26 13:08:08 +0000

    Changed color to colour (trivial, ignore this for contributions)

--- src/Key.java
+++ src/Key.java
@@ -1,21 +1,21 @@
 /**
  * Represents a key item.
  *
- * Keys can be collected by the player and used to unlock matching colored locked doors.
- * Each key has a specific color that corresponds to a particular locked door.
+ * Keys can be collected by the player and used to unlock matching coloured locked doors.
+ * Each key has a specific colour that corresponds to a particular locked door.
  *
  * @author Tafara Gonese, Joseph Vinson
  * Check Section 3.1 of the Functional Specification for detailed key and locked door mechanics
  */
 
 public class Key extends Tile {
-    /** The color of the key which determines which locked door it can open. */
+    /** The colour of the key which determines which locked door it can open. */
     private final Colour colour;
 
     /**
-     * Constructs a new Key with a specific color and position.
+     * Constructs a new Key with a specific colour and position.
      *
-     * @param colour The color of the key
+     * @param colour The colour of the key
      * @param x The x-coordinate of the key on the game map
      * @param y The y-coordinate of the key on the game map
      */
@@ -25,9 +25,9 @@ public class Key extends Tile {
     }
 
     /**
-     * Gets the color of the key.
+     * Gets the colour of the key.
      *
-     * @return The color of the key
+     * @return The colour of the key
      */
     public Colour getColour() {
         return colour;
@@ -37,7 +37,7 @@ public class Key extends Tile {
      * Checks if this key can unlock a specific locked door.
      *
      * @param lockedDoor The locked door to check against
-     * @return true if the key's color matches the door's color, false otherwise
+     * @return true if the key's colour matches the door's colour, false otherwise
      */
     public boolean canUnlock(LockedDoor lockedDoor) {
         return lockedDoor.getColour() == this.colour;
@@ -45,7 +45,7 @@ public class Key extends Tile {
 
     /**
      * Attempts to use the key to unlock a specific locked door.
-     * If the key's color matches the door's color, the door is unlocked.
+     * If the key's colour matches the door's colour, the door is unlocked.
      *
      * @param lockedDoor The locked door to attempt to unlock
      */
--- src/LockedDoor.java
+++ src/LockedDoor.java
@@ -1,7 +1,7 @@
 /**
  * Represents a locked door tile.
  *
- * Locked doors are a type of wall that can be opened by a key of a matching color.
+ * Locked doors are a type of wall that can be opened by a key of a matching colour.
  * Initially acts as a wall, but can be unlocked to become a walkable path.
  *
  * @author Joseph Vinson, Tafara Gonese
@@ -10,15 +10,15 @@
  */
 
 public class LockedDoor extends Wall {
-    /** The color of the locked door which determines which key can open it. */
+    /** The colour of the locked door which determines which key can open it. */
     private final Colour colour;
 
     /**
-     * Constructs a new Locked Door with a specific position and color.
+     * Constructs a new Locked Door with a specific position and colour.
      *
      * @param x The x-coordinate of the locked door on the game map
      * @param y The y-coordinate of the locked door on the game map
-     * @param colour The color of the locked door
+     * @param colour The colour of the locked door
      */
     public LockedDoor(int x, int y, Colour colour) {
         super(x, y, WallType.LOCKED_DOOR);
@@ -29,7 +29,7 @@ public class LockedDoor extends Wall {
      * Checks if the given key can unlock this door.
      *
      * @param key The key attempting to unlock the door
-     * @return true if the key's color matches the door's color, false otherwise
+     * @return true if the key's colour matches the door's colour, false otherwise
      */
     public boolean canUnlock(Key key) {
         return this.colour == key.getColour();
@@ -44,9 +44,9 @@ public class LockedDoor extends Wall {
     }
 
     /**
-     * Gets the color of the locked door.
+     * Gets the colour of the locked door.
      *
-     * @return The color of the door
+     * @return The colour of the door
      */
     public Colour getColour() {
         return this.colour;
