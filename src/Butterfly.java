609bac256eea93fb8d7e11f252d171b68d8f12d9
Orpheus-Picaro <142040969+Orpheus-Picaro@users.noreply.github.com>
2024-11-26 11:29:08 +0000

    create the base butterfly
    create the base butterfly with notes on how it needs to differ

---
+++ src/Butterfly.java
@@ -0,0 +1,51 @@
+import javafx.scene.image.Image;
+
+public class Butterfly extends Firefly {
+
+    /**
+     * pending image
+     *
+     * @return Image of butterfly
+     */
+
+    protected Butterfly(Image image, int[] position) {
+        super(image, position);
+    }
+
+    /**
+     * A Test method designed to be used inside moveTo to test your movement is working correctly
+     */
+
+    @Override
+    public int[] moveTo(int[][] gameState) {
+        return new int[0];
+    }
+
+    
+
+    @Override
+    public void movementTests() {
+
+    }
+
+    /**
+     * Performs any actions done when an enemy dies by a hazard and returns what they should drop on their death
+     * @return int representing a particular item or set of items to be dropped on enemy death
+	 * butterfly needs to differ from other enemies by dropping diamonds 
+     */
+
+    @Override
+    public int onDeathByHazard() {
+        return 0;
+    }
+    
+    /**
+     * performs actions done when enemies die by falling objects for most this will replace a 3 by 3 area with path excluding exeption objects of titanium walls and the exit
+     *  butterfly will differ by replacing the path with diamonds instead
+     */
+
+    @Override
+    public int onDeathByFallingObject() {
+        return 0;
+    }
+}
