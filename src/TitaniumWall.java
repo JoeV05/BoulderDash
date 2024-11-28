f178fb425d82467eaabc1412c147de5fd8a47e56
Brucee-ee <153196545+Brucee-ee@users.noreply.github.com>
2024-11-28 22:52:06 +0000

    Added basic TitaniumWall class

---
+++ src/TitaniumWall.java
@@ -0,0 +1,10 @@
+import javafx.scene.image.Image;
+
+/**
+ * @author Luke Brace
+ */
+public class TitaniumWall extends Wall{
+
+    public TitaniumWall(int x, int y, Image image) {super(x, y, WallType.TITANIUM_WALL, image);}
+    
+}
