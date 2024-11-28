a4d322099126b7912cfd392dacfd84a63ce32aa7
Brucee-ee <153196545+Brucee-ee@users.noreply.github.com>
2024-11-28 23:07:59 +0000

    Added Basic Functionality & Comments

--- src/TitaniumWall.java
+++ src/TitaniumWall.java
@@ -3,8 +3,36 @@ import javafx.scene.image.Image;
 /**
  * @author Luke Brace
  */
+
 public class TitaniumWall extends Wall{
 
-    public TitaniumWall(int x, int y, Image image) {super(x, y, WallType.TITANIUM_WALL, image);}
-    
+    /**
+     * Constructs a new Titanium Wall
+     * Titanium walls cannot be destroyed
+     *
+     * @param x The x-coordinate of the Titanium Wall on the game map
+     * @param y The y-coordinate of the Titanium Wall on the game map
+     * @param image The image representing the Titanium Wall
+     */
+    public TitaniumWall(int x, int y, Image image) {
+        super(x, y, WallType.TITANIUM_WALL, image);
+    }
+
+    /**
+     * Makes the Wall unwalkable
+     *
+     * @return The state of the wall
+     */
+    @Override
+    public boolean isWalkable() {
+        return false;
+    }
+    /**
+     * Makes the Wall indestructible
+     *
+     * @return The state of the wall
+     */
+    public boolean isDestructable(){
+        return false;
+    }
 }
