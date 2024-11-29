28ab8ea944df2eca52803d696344c5eebfb844a3
Brucee-ee <153196545+Brucee-ee@users.noreply.github.com>
2024-11-29 00:37:00 +0000

    Update Diamond.java

--- src/Diamond.java
+++ src/Diamond.java
@@ -5,8 +5,22 @@ import javafx.scene.image.Image;
  */
 
 public class Diamond extends FallingEntity {
-
+    /**
+     *
+     * @param x The x-coordinate of the Titanium Wall on the game map
+     * @param y The y-coordinate of the Titanium Wall on the game map
+     * @param image The image representing the Diamond
+     */
     public Diamond(int x, int y, Image image) {
         super(x, y, FallingType.DIAMOND,image);
+    }
+
+    /**
+     *
+     * @param
+     */
+    public void Collect (Player player) {
+        //Remove the object
+        //Give to Player
     }
 }
