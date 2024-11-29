6224f1052251e1f74aa159a87c00dc9f439260b5
Joe Devlin <2328190@swansea.ac.uk>
2024-11-29 22:03:25 +0000

    Files backed-up check the discord

--- src/FallingEntity.java
+++ src/FallingEntity.java
@@ -1,4 +1,5 @@
 import javafx.scene.image.Image;
+
 /**
  * @author Joseph Vinson
  */
@@ -6,8 +7,8 @@ import javafx.scene.image.Image;
 public abstract class FallingEntity extends Entity {
     private final FallingType entityType;
 
-    public FallingEntity(int x, int y, FallingType entityType, Image image) {
-        super(x, y,image);
+    public FallingEntity(int x, int y, FallingType entityType, Image sprite) {
+        super(x, y, sprite);
 
         this.entityType = entityType;
     }
@@ -17,8 +18,10 @@ public abstract class FallingEntity extends Entity {
     }
 
     public void fall() {
-        //TODO - use replaceEntity to move it to the tile below, assuming the tile below is empty
-        //TODO - use transform from MagicWall if the tile below isInstanceOf(MagicWall)
-        //TODO - handle case where tile below isInstanceOf(Enemy)
+        /*TODO - use Game.replaceEntity() [unsure of exact implementation]
+            to move it to the tile below, assuming the tile below is either empty or a magic wall
+            [needs to handle magic walls differently, i.e. use transform method from magic wall]
+            also somehow kill the player and enemies
+         */
     }
 }
--- src/MagicWall.java
+++ src/MagicWall.java
@@ -8,29 +8,20 @@ public class MagicWall extends Wall {
     private boolean isActive;
     private FallingEntity stored;
 
-    public MagicWall(int x, int y,Image image) {
-        super(x, y, WallType.MAGIC_WALL,image);
+    public MagicWall(int x, int y) {
+        super(x, y, WallType.MAGIC_WALL);
     }
 
     public void transform(FallingEntity falling) {
         if (falling.getEntityType() == FallingType.BOULDER) {
-            this.stored = new Diamond(this.x, this.y,new Image("tiles/diamond.png"));
+            this.stored = new Diamond(this.x, this.y);
         } else {
-            this.stored = new Boulder(this.x, this.y,new Image("tiles/boulder.png"));
+            this.stored = new Boulder(this.x, this.y);
         }
         this.isActive = true;
     }
 
     public void dropStored() {
-        // TODO - If the tile below is empty somehow drop the stored FallingEntity
-        if (this.x < 23) {// TODO - ask James if getEntity() should be static so it can be used here
-            // TODO - drop stored into tile below, update x and y for stored (y should increase by 1)
-        } else if (true) { // TODO - check if tile below is magic wall
-            // TODO - somehow chain the effect of magic wall transformation
-        }
-    }
-
-    public void chainMagicWalls() {
-        // TODO - Maybe magic walls can be chained together? (not in spec but as additional feature)
+        //Game.???(this.stored)
     }
 }
