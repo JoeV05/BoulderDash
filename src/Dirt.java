36b6c637216516b2fb6a0f9492d2dc8672a587e9
JoeV05 <josephvinson183@gmail.com>
2024-12-02 10:16:43 +0000

    Added TODO comment, removed bad method

--- src/Dirt.java
+++ src/Dirt.java
@@ -1,5 +1,7 @@
 import javafx.scene.image.Image;
 
+// TODO - proper class comment
+
 /**
  * @author Joseph Vinson
  */
@@ -7,10 +9,6 @@ import javafx.scene.image.Image;
 public class Dirt extends Tile {
 
     public Dirt(int x, int y) {
-        super(x, y, false, TileType.DIRT, new Image("./sprites/dirt.png"));
-    }
-
-    public void dig() {
-
+        super(x, y, false, TileType.DIRT, new Image("./sprites/Dirt.png"));
     }
 }
