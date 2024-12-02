aa4064bb9fccf3c079773a0042ec52009ff5b0de
JoeV05 <josephvinson183@gmail.com>
2024-12-02 10:30:23 +0000

    Added TODO comments, added sprite to constructor

--- src/MagicWall.java
+++ src/MagicWall.java
@@ -9,9 +9,10 @@ public class MagicWall extends Wall {
     private FallingEntity stored;
 
     public MagicWall(int x, int y) {
-        super(x, y, WallType.MAGIC_WALL);
+        super(x, y, WallType.MAGIC_WALL, new Image("./sprites/Magic_Wall.png"));
     }
 
+    // TODO - think this is actually all fine, maybe a sanity check from Jamie and Tafara?
     public void transform(FallingEntity falling) {
         if (falling.getEntityType() == FallingType.BOULDER) {
             this.stored = new Diamond(this.x, this.y);
@@ -21,6 +22,9 @@ public class MagicWall extends Wall {
         this.isActive = true;
     }
 
+    // TODO - Shoot me in the head, this game is a fucking nightmare
+    //  Make the magic wall either drop the FallingEntity it has stored or transfer it into a MagicWall
+    //  if there is one beneath this one (make use of static methods in Game)
     public void dropStored() {
         // TODO - If the tile below is empty somehow drop the stored FallingEntity
         if (this.x < 23) {// TODO - ask James if getEntity() should be static so it can be used here
