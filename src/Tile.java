1107017fe6c7f3261f63b975406faf7a71113590
JoeV05 <josephvinson183@gmail.com>
2024-12-02 10:33:10 +0000

    Added TODO comments

--- src/Tile.java
+++ src/Tile.java
@@ -1,4 +1,7 @@
 import javafx.scene.image.Image;
+
+// TODO - reconsider whether this class needs to exist under an entity hierarchy
+
 /**
  * @author Joseph Vinson
  * Represents a generic tile in the game
@@ -8,17 +11,18 @@ import javafx.scene.image.Image;
  */
 
 public abstract class Tile extends Entity{
-    protected boolean walkable;
+    protected boolean walkable; //Indicates whether the tile can be walked on
 
-    protected final TileType tileType;
+    protected final TileType tileType; //Indicates what type of tile it is
 
-    public Tile(int x, int y, boolean walkable, TileType tileType, Image sprite) {
-        super(x, y, sprite);
+    public Tile(int x, int y, boolean walkable, TileType tileType, Image image) {
+        super(x, y, image); //calls the Entity constructor to set the tiles coordinates
         this.walkable = walkable;
         this.tileType = tileType;
     }
 
+    // TODO - javadoc method comment
     public boolean isWalkable() {
-        return walkable;
+        return walkable; //Returns true if the tile is walkable, returns false if not
     }
 }
