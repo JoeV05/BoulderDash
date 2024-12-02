fc0929bd569fd5054757a7feede940ce3f76d77b
JoeV05 <josephvinson183@gmail.com>
2024-12-02 10:37:04 +0000

    Moved javadoc comments to correct position

--- src/WallType.java
+++ src/WallType.java
@@ -4,27 +4,27 @@
  */
 
 public enum WallType {
-    NORMAL_WALL,
     /**
      * A standard wall that blocks all movement. Normal walls are destructible
      * by explosions and can be replaced by a path tile when destroyed.
      */
-    TITANIUM_WALL,
+    NORMAL_WALL,
     /**
      * An indestructible wall that blocks all movement. Titanium walls cannot
      * be destroyed by explosions, making them permanent obstacles.
      */
-    MAGIC_WALL,
+    TITANIUM_WALL,
     /**
      * A special wall that transforms falling objects:
      * Boulders entering a magic wall become diamonds.
      * Diamonds entering a magic wall become boulders.
      * Magic walls block movement but are vulnerable to explosions, which destroy them.
      */
-    LOCKED_DOOR
+    MAGIC_WALL,
     /**
      * Acts as a titanium wall until unlocked by the player using
      * The corresponding key. Once unlocked, the door transforms into
      * a walkable path tile
      */
+    LOCKED_DOOR
 }
