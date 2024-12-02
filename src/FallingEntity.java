9a74beb5b5b6fa590d5a91479eea0f279e931b30
JoeV05 <josephvinson183@gmail.com>
2024-12-02 17:04:34 +0000

    Started falling behaviour

--- src/FallingEntity.java
+++ src/FallingEntity.java
@@ -21,7 +21,17 @@ public abstract class FallingEntity extends Entity {
     }
 
     public void fall() {
-        //TODO - use replaceEntity to move it to the tile below, assuming the tile below is either empty or a magic wall
+        if (this.y > Game.GRID_HEIGHT) {
+            throw new LiamWetFishException("HOT SINGLE FISH IN YOUR AREA");
+        }
+        if (this.y == Game.GRID_HEIGHT - 1) {
+            return;
+        }
+        if (Game.getEntity(this.x, this.y + 1) instanceof Path) {
+            Game.updateLevel(x, y + 1, this);
+        }
+        //TODO - use updateLevel to move it to the tile below, assuming the tile below is either empty or a magic wall
         //TODO - handle case where entity below is either player or enemy
+        //  ^^^ This is for hazards people, not tiles, stop trying to make me do your work for you
     }
 }
