c8b508a750a7d2444b10dbc1857cd83b4e2727af
JoeV05 <josephvinson183@gmail.com>
2024-12-02 17:51:12 +0000

    Added proper sprite switching via static method

--- src/Wall.java
+++ src/Wall.java
@@ -9,16 +9,30 @@ import  javafx.scene.image.Image;
 public class Wall extends Tile {
     protected WallType wallType; // The specific type of this wall (e.g., Normal, Titanium, Magic, Locked Door).
 
-    // TODO - is sprite switching already handled?
-    //  I don't remember but it looks like it?
     public Wall(int x, int y, WallType wallType) {
-        super(x, y, false, TileType.WALL, new Image("./sprites/Wall.png")); //this type of tile is not walkable and it belongs to the wall type
+        super(x, y, false, TileType.WALL, Wall.getWallSprite(wallType)); //this type of tile is not walkable and it belongs to the wall type
 
         this.wallType = wallType; //sets the type of wall i.e. magic, titanium,door
     }
 
-    public Wall(int x, int y, WallType wallType, Image sprite) {
-        super(x, y, false, TileType.WALL, sprite);
-        this.wallType = wallType;
+    private static Image getWallSprite(WallType wallType) {
+        Image img;
+        switch (wallType) {
+            case WallType.NORMAL_WALL:
+                img = new Image("./sprites/Wall.png");
+                break;
+            case WallType.TITANIUM_WALL:
+                img = new Image("./sprites/Titanium.png");
+                break;
+            case WallType.MAGIC_WALL:
+                img = new Image("./sprites/magic_wall.png");
+                break;
+            case WallType.EXIT:
+                img = new Image("./sprites/Exit_Locked.png");
+            default:
+                throw new LiamWetFishException("WHAT THE FISH??? WHERES THE FISHING WALLTYPE???");
+        }
+        return img;
     }
+
 }
