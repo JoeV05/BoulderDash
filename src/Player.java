fbe42be664fce356fe59d68d4fce7438ca23b5af
JoeV05 <josephvinson183@gmail.com>
2024-12-02 16:41:09 +0000

    Updated player movement so if you try to move at the edge you will turn but not move in edge direction

--- src/Player.java
+++ src/Player.java
@@ -19,45 +19,45 @@ public class Player extends Entity {
     public void move(KeyCode key) {
         int x = getX();
         int y = getY();
+        if (x > Game.GRID_WIDTH - 1 || x < 0 || y > Game.GRID_HEIGHT - 1 || y < 0) {
+            throw new LiamWetFishException("IM GOING TO HIT YOU WITH A WET FISH!");
+        }
         this.moveSwitch(key);
         Game.checkForChangeInView(x, y, this.getX(), this.getY());
     }
 
     // TODO - javadoc method comment
+    // TODO - maybe only one set of keys for moving
     private void moveSwitch(KeyCode key) {
         Path p = new Path(x, y);
         switch (key) {
             case UP:
             case W:
-                if (!Game.isValidMove(x, y, Direction.UP)) {
-                    return;
-                }
                 setSprite(new Image("./sprites/Player_Down.png"));
-                Game.updateLevel(x, y - 1, this);
+                if (Game.isValidMove(x, y, Direction.UP)) {
+                    Game.updateLevel(x, y - 1, this);
+                }
                 break;
             case DOWN:
             case S:
-                if (!Game.isValidMove(x, y, Direction.DOWN)) {
-                    return;
-                }
                 setSprite(new Image("./sprites/Player_Up.png"));
-                Game.updateLevel(x, y + 1, this);
+                if (Game.isValidMove(x, y, Direction.DOWN)) {
+                    Game.updateLevel(x, y + 1, this);
+                }
                 break;
             case LEFT:
             case A:
-                if (!Game.isValidMove(x, y, Direction.LEFT)) {
-                    return;
-                }
                 setSprite(new Image("./sprites/Player_Left.png"));
-                Game.updateLevel(x - 1, y, this);
+                if (Game.isValidMove(x, y, Direction.LEFT)) {
+                    Game.updateLevel(x - 1, y, this);
+                }
                 break;
             case RIGHT:
             case D:
-                if (!Game.isValidMove(x, y, Direction.RIGHT)) {
-                    return;
-                }
                 setSprite(new Image("./sprites/Player_Right.png"));
-                Game.updateLevel(x + 1, y, this);
+                if (Game.isValidMove(x, y, Direction.RIGHT)) {
+                    Game.updateLevel(x + 1, y, this);
+                }
                 break;
             default:
                 // all redundant key inputs are sent here
