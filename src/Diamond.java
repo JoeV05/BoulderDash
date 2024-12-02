96a80617bb17ae19e4adf221743fa907808e68d3
JoeV05 <josephvinson183@gmail.com>
2024-12-02 10:16:07 +0000

    Added random sprite choice (James Harvey)

--- src/Diamond.java
+++ src/Diamond.java
@@ -1,5 +1,11 @@
 import javafx.scene.image.Image;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+// TODO - proper class comment
+
 /**
  * @author Joseph Vinson
  */
@@ -7,6 +13,19 @@ import javafx.scene.image.Image;
 public class Diamond extends FallingEntity {
 
     public Diamond(int x, int y) {
-        super(x, y, FallingType.DIAMOND, new Image("./sprites/diamond.png"));
+        super(x, y, FallingType.DIAMOND, new Image("sprites/Diamond_Blue.png"));
+        setSprite(randomSprite());
+    }
+
+    // TODO - proper method comment
+    //Diamonds can have 1 of 4 variations, chosen at random, does not impact behaviour, only image
+    private Image randomSprite() {
+        List<Image> diamonds = new ArrayList<>();
+        diamonds.add(new Image("./sprites/Diamond_Blue.png"));
+        diamonds.add(new Image("./sprites/Diamond_Red.png"));
+        diamonds.add(new Image("./sprites/Diamond_Green.png"));
+        diamonds.add(new Image("./sprites/Diamond_White.png"));
+        Random rand = new Random();
+        return diamonds.get(rand.nextInt(diamonds.size()));
     }
 }
