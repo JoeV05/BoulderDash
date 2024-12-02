f5c4733b866ba2c93c8f124ff4d52c13855f7383
JoeV05 <josephvinson183@gmail.com>
2024-12-02 10:32:28 +0000

    Added TODO comments, set sprite urls to correct values when moving, switched from Devlin movement to Liam/James movement, made player a singleton

--- src/Player.java
+++ src/Player.java
@@ -1,40 +1,75 @@
-import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
-
+import javafx.scene.image.Image;
 import java.util.ArrayList;
+
+// TODO - javadoc class comment
 
 public class Player extends Entity {
 
     private ArrayList<Key> keys = new ArrayList<>();
     private int diamonds = 0;
 
-    public Player(int x, int y) {
-        super(x, y, new Image("./sprites/player.png"));
+    private static Player thePlayer;
+
+    private Player(int x, int y) {
+        super(x, y, new Image("./sprites/Player_Down.png"));
     }
 
-    public int[] move(KeyCode key) {
-        int dy = 0;
-        int dx = 0;
+    // TODO - javadoc method comment
+    // TODO - maybe there's already a method in Game that could simplify this?
+    //  not sure, this was a quick dirty fix to try to get player moving again
+    public void move(KeyCode key) {
+        int x = getX();
+        int y = getY();
+        Path p = new Path(x, y);
         switch (key) {
             case UP:
             case W:
-                dy = -1;
+                setSprite(new Image("./sprites/Player_Down.png"));
+                Game.replaceEntity(x, y - 1, this);
+                setY(y - 1);
+                Game.replaceEntity(x, y, p);
                 break;
             case DOWN:
             case S:
-                dy = 1;
+                setSprite(new Image("./sprites/Player_Up.png"));
+                Game.replaceEntity(x, y + 1, this);
+                setY(y + 1);
+                Game.replaceEntity(x, y, p);
                 break;
             case LEFT:
             case A:
-                dx = -1;
+                setSprite(new Image("./sprites/Player_Left.png"));
+                Game.replaceEntity(x - 1, y, this);
+                setX(x - 1);
+                Game.replaceEntity(x, y, p);
                 break;
             case RIGHT:
             case D:
-                dx = 1;
+                setSprite(new Image("./sprites/Player_Right.png"));
+                Game.replaceEntity(x + 1, y, this);
+                setX(x + 1);
+                Game.replaceEntity(x, y, p);
                 break;
             default:
+                // all redundant key inputs are sent here
                 break;
         }
-        return new int[]{dy, dx};
+        Game.checkForChangeInView(x, y, this.getX(), this.getY());
+    }
+
+    // TODO - javadoc method comment
+    public static Player getPlayer() {
+        if (thePlayer != null) {
+            return thePlayer;
+        } else {
+            return null;
+        }
+    }
+
+    // TODO - javadoc method comment
+    public static Player getPlayer(int x, int y) {
+        thePlayer = new Player(x, y);
+        return thePlayer;
     }
 }
