57bba56d21c72fafd9c496905e1af335d23ab3c6
JoeV05 <josephvinson183@gmail.com>
2024-12-02 10:10:10 +0000

    Replaced devlin movement system with what james made

--- src/Game.java
+++ src/Game.java
@@ -2,42 +2,41 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 import javafx.scene.input.*;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
-import java.util.function.BiFunction;
-import javafx.scene.image.Image;
 
 import static java.util.Objects.isNull;
 
+// TODO - Write good class comment
+
 /**
- * @author James, Luke, and Joe
- * Fixing the game class.
+ * @author James Harvey, Luke Brace, Joseph Vinson, Joe Devlin
+ * Fixing the game class. The errors are mainly due to the relationships between Tile, Wall, and Entity.
+ * ^^^ Literally not even true, what are you on about
  */
 public class Game extends Application {
 
     // title.
     private static final String GAME_TITLE = "Boulder Dash";
 
-    // canvas.
+    // window properties.
+    private static final int WINDOW_WIDTH = 750;
+    private static final int WINDOW_HEIGHT = 400;
+
+    // canvas. canvas is contained within window.
     private static final int CANVAS_WIDTH = 750;
     private static final int CANVAS_HEIGHT = 400;
     private Canvas canvas;
@@ -50,19 +49,17 @@ public class Game extends Application {
     private static final int GRID_WIDTH = 40;
     private static final int GRID_HEIGHT = 23;
 
-    // temp.
+    //TODO: Remove, set sprites to 32px
+    // TODO - There are already variables for 32px, can whoever added the comment
+    //  explain what it means on the discord
     private static final int GRID_CELL_WIDTH = 25;
     private static final int GRID_CELL_HEIGHT = 25;
 
     // entity map.
-    private static Entity[][] cave;
+    private static Entity[][] map;
 
     // timeline.
     private Timeline tickTimeline;
-
-    // player.
-    private static int rowWherePlayerIs;
-    private static int colWherePlayerIs;
 
     // control registering -> actively held keys.
     private final Queue<KeyCode> pressedKeys = new LinkedList<>();
@@ -72,26 +69,29 @@ public class Game extends Application {
     private static final View view = new View(1);
 
     /**
-     * // TODO: write javadoc when method is finalised
+     *
+     * @param primaryStage The stage that is to be used for the application.
      */
     @Override
     public void start(Stage primaryStage) throws IOException {
-        // this has to be done for some reason.
+        //Declare new canvas
         canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
 
-        // setting scene.
-        BorderPane root = (BorderPane) FXMLLoader.load(Objects.requireNonNull(getClass().getResource("layout.fxml")));
+        // setting the scene.
+        BorderPane root = (BorderPane)FXMLLoader.load(Objects.requireNonNull(getClass().getResource("layout.fxml")));
         Scene scene = new Scene(root, CANVAS_WIDTH, CANVAS_HEIGHT);
         root.setCenter(canvas);
-        // setting stage.
+
+        // setting the stage.
         primaryStage.setTitle(GAME_TITLE);
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        // setting control registering.
+        // setting control registering
         scene.addEventFilter(KeyEvent.KEY_PRESSED, this::handleKeyPressed);
         scene.addEventFilter(KeyEvent.KEY_RELEASED, this::handleKeyReleased);
 
+        // load the cave.
         loadingCave();
 
         // setting tick-based system.
@@ -100,7 +100,8 @@ public class Game extends Application {
         tickTimeline.play();
     }
 
-    // handling non-standard key-to-action relationships.
+    // TODO - add useful comment
+    // TODO - devlin said this would have uses outside of movement (i.e. pausing the game)
     public void handleKeyPressed(KeyEvent event) {
         if (!seenKeys.contains(event.getCode())) {
             pressedKeys.add(event.getCode());
@@ -115,8 +116,9 @@ public class Game extends Application {
     }
 
     public void loadingCave() throws FileNotFoundException {
-        Cave charCave = new Cave("Regular Cave", "level-1.txt");
-        cave = new Entity[charCave.getTilesTall()][charCave.getTilesWide()];
+        Cave charCave = new Cave("Cave1", "level-1.txt");
+        //TODO: Add automatic cave generation, see cave class
+        map = new Entity[charCave.getTilesTall()][charCave.getTilesWide()];
         charCave.printCave();
         char[][] reduceGets = charCave.getCave();
 
@@ -124,36 +126,37 @@ public class Game extends Application {
             for (int col = 0; col < charCave.getTilesWide(); col++) {
                 char tileChar = reduceGets[row][col];
 
-                cave[row][col] = switch (tileChar) {
-                    case '#' -> new Wall(row, col, WallType.NORMAL_WALL);
-                    case 'T' -> new Wall(row, col, WallType.TITANIUM_WALL);
-                    case 'M' -> new MagicWall(row, col);
-                    case 'E' -> new Exit(row, col, 5);
-                    case 'R' -> new LockedDoor(row, col, Colour.RED);
-                    case 'G' -> new LockedDoor(row, col, Colour.GREEN);
-                    case 'B' -> new LockedDoor(row, col, Colour.BLUE);
-                    case 'Y' -> new LockedDoor(row, col, Colour.YELLOW);
-                    case 'r' -> new Key(row, col, Colour.RED);
-                    case 'g' -> new Key(row, col, Colour.GREEN);
-                    case 'b' -> new Key(row, col, Colour.BLUE);
-                    case 'y' -> new Key(row, col, Colour.YELLOW);
-                    case 'P' -> new Player(row, col);
-                    case 'O' -> new Boulder(row, col);
-                    case 'V' -> new Diamond(row, col);
-                    case 'W' -> new Butterfly(row, col);
-                    case 'X' -> new Firefly(row, col);
-                    case 'F' -> new Frog(row, col);
-                    case 'A' -> new Amoeba(row, col, 10, 10);
-                    default -> new Dirt(row, col);
+                map[row][col] = switch(tileChar) {
+                    case '#' -> new Wall(col, row, WallType.NORMAL_WALL);
+                    case 'T' -> new Wall(col, row, WallType.TITANIUM_WALL);
+                    case 'M' -> new Wall(col, row, WallType.MAGIC_WALL);
+                    case 'E' -> new Exit(col, row, 5);
+                    // TODO - metadata needed for unlock exit condition
+                    case 'R' -> new LockedDoor(col, row, Colour.RED);
+                    case 'G' -> new LockedDoor(col, row, Colour.GREEN);
+                    case 'B' -> new LockedDoor(col, row, Colour.BLUE);
+                    case 'Y' -> new LockedDoor(col, row, Colour.YELLOW);
+                    case 'r' -> new Key(col, row, Colour.RED);
+                    case 'g' -> new Key(col, row, Colour.GREEN);
+                    case 'b' -> new Key(col, row, Colour.BLUE);
+                    case 'y' -> new Key(col, row, Colour.YELLOW);
+                    case 'O' -> new Boulder(col, row);
+                    case 'V' -> new Diamond(col, row);
+                    case 'W' -> new Butterfly(col, row);
+                    case 'X' -> new Firefly(col, row);
+                    // TODO - metadata needed for left/right wall cling
+                    case 'F' -> new Frog(col, row);
+                    case 'A' -> new Amoeba(col, row, 10, 10);
+                    case 'P' -> Player.getPlayer(col, row);
+                    // TODO - metadata needed for maximum Amoeba size
+                    default -> new Dirt(col, row);
                 };
-                if (tileChar == 'P') {
-                    rowWherePlayerIs = row;
-                    colWherePlayerIs = col;
-                }
             }
         }
     }
 
+    // TODO - Figure it where this is meant to be used
+    //  Also shouldn't it replace with a path and not dirt?
     /**
      * Change the position of an Entity in the levelState
      */
@@ -171,39 +174,31 @@ public class Game extends Application {
      * @param entity to be replaced with
      */
     public static void replaceEntity(int x, int y, Entity entity) {
-        //levelState[y][x] = entity;
+        map[y][x] = entity;
     }
 
-    // made this for no reason
+    /**
+     * Returns the entity at the given x/y coordinates
+     * @return Entity to be moved
+     */
+    public Entity getEntity(int x, int y) {
+        return map[y][x];
+    }
+
+    // TODO - what is this for? looks like an artefact of devlin shitfuckery based off the use of deltas
     public static void swapEntities(int dy, int dx) {
         boolean moved = !(dy == 0 && dx == 0);
         if (moved) {
-            Entity a = cave[rowWherePlayerIs][colWherePlayerIs];
-            Entity b = cave[rowWherePlayerIs+dy][colWherePlayerIs+dx];
+            Player p = Player.getPlayer();
+            Entity a = map[p.getY()][p.getX()];
+            Entity b = map[p.getY() +dy][p.getX() +dx];
             Entity c;
             c = a;
-            cave[rowWherePlayerIs][colWherePlayerIs] = cave[rowWherePlayerIs+dy][colWherePlayerIs+dx];
-            cave[rowWherePlayerIs+dy][colWherePlayerIs+dx] = c;
+            map[p.getY()][p.getX()] = map[p.getY() +dy][p.getX() +dx];
+            map[p.getY() +dy][p.getX() +dx] = c;
 
-            rowWherePlayerIs += dy;
-            colWherePlayerIs += dx;
-        }
-    }
-
-    public static void pathTrail(int dy, int dx) {
-        boolean moved = !(dy == 0 && dx == 0);
-        if (moved) {
-            boolean onMap = !(isNull(cave[rowWherePlayerIs+dy][colWherePlayerIs+dx]));
-            if (onMap) {
-                Entity a = cave[rowWherePlayerIs][colWherePlayerIs];
-                cave[rowWherePlayerIs][colWherePlayerIs] = new Path(rowWherePlayerIs, colWherePlayerIs);
-                cave[rowWherePlayerIs+dy][colWherePlayerIs+dx] = a;
-
-                checkForChangeInView(colWherePlayerIs, rowWherePlayerIs, colWherePlayerIs+dx, rowWherePlayerIs+dy);
-
-                rowWherePlayerIs += dy;
-                colWherePlayerIs += dx;
-            }
+            p.setY(p.getY() + dy);
+            p.setX(p.getX() + dx);
         }
     }
 
@@ -215,14 +210,15 @@ public class Game extends Application {
      */
     public void tick() {
         if (!pressedKeys.isEmpty()) {
+            Player p = Player.getPlayer();
             KeyCode intendedKey = pressedKeys.peek();
-            Player player = (Player) cave[rowWherePlayerIs][colWherePlayerIs];
-            int[] diff = player.move(intendedKey);
-            pathTrail(diff[0], diff[1]);
+            p.move(intendedKey);
         }
         draw();
     }
 
+    // TODO - javadoc comment
+    // TODO - display the score
     private void draw() {
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
@@ -239,19 +235,26 @@ public class Game extends Application {
 
         for (int y = yStart; y <= yEnd; y++) {
             for (int x = xStart; x <= xEnd; x++) {
-                mapWeCanSee[y - yStart][x - xStart] = cave[y][x];
+                mapWeCanSee[y - yStart][x - xStart] = map[y][x];
             }
         }
 
         for (int y = 0; y < 16; y++) {
             for (int x = 0; x < 30; x++) {
                 Entity entity = mapWeCanSee[y][x];
-                javafx.scene.image.Image sprite = entity.getSprite();
+                Image sprite = entity.getSprite();
                 gc.drawImage(sprite, x * GRID_CELL_WIDTH, y * GRID_CELL_HEIGHT, GRID_CELL_WIDTH, GRID_CELL_HEIGHT);
             }
         }
     }
 
+    /**
+     * Shifts the screen to centre the player
+     * @param xBefore x-position before player movement
+     * @param yBefore y-position before player movement
+     * @param playerX x-position the player is currently in
+     * @param playerY y-position the player is currently in
+     */
     public static void checkForChangeInView(int xBefore, int yBefore, int playerX, int playerY) {
         if (view.getView() == 1) {
             if (xBefore == 24 && playerX == 25) {
@@ -262,10 +265,14 @@ public class Game extends Application {
         } else if (view.getView() == 2) {
             if (xBefore == 17 && playerX == 16) {
                 view.changeViewMode(1);
+            } else if (yBefore == 13 && playerY == 14) {
+                view.changeViewMode(4);
             }
         } else if (view.getView() == 3) {
             if (xBefore == 24 && playerX == 25) {
                 view.changeViewMode(4);
+            } else if (yBefore == 8 && playerY == 7) {
+                view.changeViewMode(1);
             }
         } else if (view.getView() == 4) {
             if (xBefore == 17 && playerX == 16) {
@@ -275,6 +282,7 @@ public class Game extends Application {
             }
         }
     }
+
     public static void main(String[] args) {
         launch(args);
     }
