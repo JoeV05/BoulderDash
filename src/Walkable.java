364e54627bb78769a01c4b338d1c30507c487292
JoeV05 <josephvinson183@gmail.com>
2024-12-03 18:23:11 +0000

    Made new class for walkable entities

---
+++ src/Walkable.java
@@ -0,0 +1,13 @@
+import javafx.scene.image.Image;
+
+// TODO - javadoc class comment
+
+/**
+ * @author Joseph Vinson
+ */
+public abstract class Walkable extends Entity {
+
+    public Walkable(int x, int y, Image sprite) {
+        super(x, y, sprite);
+    }
+}
