18cc07ef81a24d157ff12b96c84cf3f58552ba4c
Orpheus-Picaro <142040969+Orpheus-Picaro@users.noreply.github.com>
2024-12-05 13:19:22 +0000

    Update Butterfly.java
    death method for butterfly

--- src/Butterfly.java
+++ src/Butterfly.java
@@ -37,8 +37,87 @@ public class Butterfly extends Firefly {
      *  butterfly will differ by replacing the path with diamonds instead
      */
 
-    @Override
-    public int onDeathByFallingObject() {
+     @Override
+    public int onDeathByFallingObject(Entity below) {
+        int positionX = below.getX();
+        int positionY= below.getY();
+        if(checker(positionX, positionY) == true){
+            Diamond dropedDiamond = new Diamond(x, y);
+            Game.getGame().replaceEntity(positionX, positionY, dropedDiamond);
+            positionX= positionX+1;
+        }else{
+            positionX= positionX+1;
+        }
+        if(checker(positionX, positionY) == true){
+            Diamond dropedDiamond = new Diamond(x, y);
+            Game.getGame().replaceEntity(positionX, positionY, dropedDiamond);
+            positionY= positionY+1;
+        }else{
+            positionY= positionY+1;
+        }
+        if(checker(positionX, positionY) == true){
+            Diamond dropedDiamond = new Diamond(x, y);
+            Game.getGame().replaceEntity(positionX, positionY, dropedDiamond);
+            positionX= positionX-1;
+        }else{
+            positionX= positionX-1;
+        }
+        if(checker(positionX, positionY) == true){
+            Diamond dropedDiamond = new Diamond(x, y);
+            Game.getGame().replaceEntity(positionX, positionY, dropedDiamond);
+            positionX= positionX-1;
+        }else{
+            positionX= positionX-1;
+        }
+        if(checker(positionX, positionY) == true){
+            Diamond dropedDiamond = new Diamond(x, y);
+            Game.getGame().replaceEntity(positionX, positionY, dropedDiamond);
+            positionY= positionY-1;
+        }else{
+            positionY= positionY-1;
+        }
+        if(checker(positionX, positionY) == true){
+            Diamond dropedDiamond = new Diamond(x, y);
+            Game.getGame().replaceEntity(positionX, positionY, dropedDiamond);
+            positionY= positionY-1;
+        }else{
+            positionY= positionY-1;
+        }
+        if(checker(positionX, positionY) == true){
+            Diamond dropedDiamond = new Diamond(x, y);
+            Game.getGame().replaceEntity(positionX, positionY, dropedDiamond);
+            positionX= positionX+1;
+        }else{
+            positionX= positionX+1;
+        }
+        if(checker(positionX, positionY) == true){
+            Diamond dropedDiamond = new Diamond(x, y);
+            Game.getGame().replaceEntity(positionX, positionY, dropedDiamond);
+            positionX= positionX+1;
+        }else{
+            positionX= positionX+1;
+        }
         return 0;
+    }
+
+    @Override
+    public boolean checker(int x, int y) {
+        Entity check = Game.getGame().getEntity(x, y);
+        if (check instanceof Exit ){
+            return false;
+        } else if(check instanceof Wall){
+            WallType notUnbreakable =((Wall)check).getWallType();
+            if (notUnbreakable == WallType.TITANIUM_WALL){
+                return false;
+            }else{
+                return true;
+            }  
+        } else if (check instanceof Butterfly){
+            Diamond dropedDiamond = new Diamond(x, y);
+            Game.getGame().replaceEntity(x, y, dropedDiamond);
+            return false;
+        } else{
+            return true;
+        }
     }
 }
