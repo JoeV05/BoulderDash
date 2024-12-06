df1352b052a1640e025e6ec3d23ccd00eda5f938
JoeV05 <josephvinson183@gmail.com>
2024-12-06 14:53:08 +0000

    Rolling behaviour implemented

--- src/FallingEntity.java
+++ src/FallingEntity.java
@@ -1,5 +1,7 @@
 import javafx.scene.image.Image;
 
+import java.util.Random;
+
 // TODO - proper javadoc comment
 
 /**
@@ -8,11 +10,15 @@ import javafx.scene.image.Image;
 
 public abstract class FallingEntity extends Entity {
     private final FallingType entityType;
+    protected boolean falling;
+
+    public static final int[] ROLLING_CHOICES = {-1, 1};
 
     public FallingEntity(int x, int y, FallingType entityType, Image image) {
         super(x, y,image);
 
         this.entityType = entityType;
+        this.falling = false;
     }
 
     // TODO - is this method necessary
@@ -23,7 +29,7 @@ public abstract class FallingEntity extends Entity {
 
     // TODO - javadoc method comment
     public void fall() {
-        Game game = Game.getGame();
+        ///Game game = Game.getGame();
         if (this.y > Game.GRID_HEIGHT) {
             throw new LiamWetFishException("HOT SINGLE FISH IN YOUR AREA");
         }
@@ -31,14 +37,37 @@ public abstract class FallingEntity extends Entity {
         if (this.y == Game.GRID_HEIGHT - 2) {
             return;
         }
-        Entity below = game.getEntity(this.x, this.y + 1);
+        Entity below = Game.getGame().getEntity(this.x, this.y + 1);
+        if (!(below instanceof Path || below instanceof MagicWall)) {
+            if (this.x < Game.GRID_WIDTH - 1 && Game.getGame().getEntity(this.x + 1, this.y) instanceof Path && this.falling) {
+                if (this.x > 0 && Game.getGame().getEntity(this.x - 1, this.y) instanceof Path) {
+                    Random r = new Random();
+                    int dX = FallingEntity.ROLLING_CHOICES[r.nextInt(FallingEntity.ROLLING_CHOICES.length)];
+                    Game.getGame().updateLevel(this.x + dX, this.y, this);
+                    this.falling = false;
+                    return;
+                }
+                int nX = this.x + 1;
+                Game.getGame().updateLevel(nX, this.y, this);
+                this.falling = false;
+                return;
+            } else if (this.x > 0 && Game.getGame().getEntity(this.x - 1, this.y) instanceof Path && falling) {
+                int nX = this.x - 1;
+                Game.getGame().updateLevel(nX, this.y, this);
+                this.falling = false;
+                return;
+            }
+        }
         if (below instanceof Path) {
             Game.getGame().updateLevel(x, y + 1, this);
+            this.falling = true;
+            return;
         }
         if (below instanceof MagicWall) {
             ((MagicWall) below).transform(this);
+            return;
         }
+        this.falling = false;
         //TODO - handle case where entity below is either player or enemy
-        //  ^^^ This is for hazards people, not tiles, stop trying to make me do your work for you
     }
 }
