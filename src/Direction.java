7c39017aace300496cf5f7ddf951d4d826ee1c9a
JoeV05 <josephvinson183@gmail.com>
2024-12-07 13:02:24 +0000

    Modified to fit CS-230 coding conventions

--- src/Direction.java
+++ src/Direction.java
@@ -1,9 +1,12 @@
-// TODO - javadoc comments
-
+/**
+ * Enumeration for the different directions the player can move in.
+ * @author Joseph Vinson
+ * @version 1.1
+ */
 public enum Direction {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT,
-    NONE
+    UP, //Represents upwards movement
+    DOWN, //Represents downwards movement
+    LEFT, //Represents leftwards movement
+    RIGHT, //Represents rightwards movement
+    NONE //Represents no movement
 }
