4aeccc1b1cf616b7f01470e5bb4aacb2d87a759f
JoeV05 <josephvinson183@gmail.com>
2024-12-07 15:17:15 +0000

    Modified to fit CS-230 coding conventions

--- src/Key.java
+++ src/Key.java
@@ -1,35 +1,33 @@
 import javafx.scene.image.Image;
 
 /**
- * Represents a key item.
- *
- * Keys can be collected by the player and used to unlock matching coloured locked doors.
- * Each key has a specific colour that corresponds to a particular locked door.
- *
+ * Represents a key within the game. Keys can be collected by the player and
+ * used to unlock matching coloured locked doors. Each key has a specific
+ * colour that corresponds to a particular locked door.
  * @author Tafara Gonese, Joseph Vinson
- * Check Section 3.1 of the Functional Specification for detailed key and locked door mechanics
  */
-
 public class Key extends Walkable {
-    /** The colour of the key which determines which locked door it can open. */
+    //The colour of the key which determines which locked door it can open.
     private final Colour colour;
 
-    // TODO replace placeholder parameter and switch statement with static method
     /**
      * Constructs a new Key with a specific colour and position.
-     *
      * @param colour The colour of the key
      * @param x The x-coordinate of the key on the game map
      * @param y The y-coordinate of the key on the game map
      */
     public Key(int x, int y, Colour colour) {
-        super(x, y, new Image("sprites/key_blue.png"));
+        super(x, y, spriteSwitch(colour));
         this.colour = colour;
     }
 
-    // TODO - javadoc method comment
-    private Image spriteSwitch(Colour colour) {
-        switch(colour) {
+    /**
+     * Choose the correct sprite for a key of a given colour.
+     * @param colour The colour of the key to get the sprite of.
+     * @return RED, YELLOW, GREEN or BLUE
+     */
+    private static Image spriteSwitch(Colour colour) {
+        switch (colour) {
             case RED:
                 return new Image("sprites/key_red.png");
             case YELLOW:
@@ -39,41 +37,24 @@ public class Key extends Walkable {
             case BLUE:
                 return new Image("sprites/key_blue.png");
             default:
-                throw new LiamWetFishException("BAD KEY COLOUR, THE FISH ARE COMING FOR YOU");
+                throw new IllegalArgumentException("Illegal colour " + colour);
         }
     }
 
     /**
      * Gets the colour of the key.
-     *
      * @return The colour of the key
      */
     public Colour getColour() {
         return colour;
     }
 
-    // TODO - shouldn't the logic be if a Player object can unlock() a Door object with one of the Key objects it has,
-    //  getColour should be sufficient for this in terms of the role of key in the operation (I think)
     /**
      * Checks if this key can unlock a specific locked door.
-     *
-     * @param lockedDoor The locked door to check against
-     * @return true if the key's colour matches the door's colour, false otherwise
+     * @param lockedDoor The locked door to check against.
+     * @return true if key colour matches the door colour, otherwise false.
      */
     public boolean canUnlock(LockedDoor lockedDoor) {
         return lockedDoor.getColour() == this.colour;
-    }
-
-    // TODO - what
-    /**
-     * Attempts to use the key to unlock a specific locked door.
-     * If the key's colour matches the door's colour, the door is unlocked.
-     *
-     * @param lockedDoor The locked door to attempt to unlock
-     */
-    public void use(LockedDoor lockedDoor) {
-        if (canUnlock(lockedDoor)) {
-            lockedDoor.unlock();
-        }
     }
 }
