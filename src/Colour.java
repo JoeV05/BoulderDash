2422d8d6f28ce6721143df6d149265ee426a874c
JoeV05 <josephvinson183@gmail.com>
2024-12-07 11:26:24 +0000

    Modified to fit CS-230 coding conventions

--- src/Colour.java
+++ src/Colour.java
@@ -1,14 +1,12 @@
-// TODO - revisit javadoc comments
-
 /**
- * Enumeration for the different colours that a key or door can have
+ * Enumeration for the different colours that a key or door can have.
  * @author Joseph Vinson
+ * @version 1.1
  */
-
 public enum Colour {
-    RED,
-    GREEN,
-    YELLOW,
-    BLUE,
-    NONE
+    RED, //Represents a red door/key
+    GREEN, //Represents a green door/key
+    YELLOW, //Represents a yellow door/key
+    BLUE, //Represents a blue door/key
+    NONE //Represents a door that has been broken open by a boulder
 }
