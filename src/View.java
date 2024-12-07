8b856fb012d4bf75da2217f2f4f5da4920e126c0
JoeV05 <josephvinson183@gmail.com>
2024-12-07 17:29:46 +0000

    Changed exception message to include exception cause

--- src/View.java
+++ src/View.java
@@ -25,7 +25,8 @@ public class View {
             case 2 -> new int[]{10, 39, 0, 15};
             case 3 -> new int[]{0, 29, 6, 21};
             case 4 -> new int[]{10, 39, 6, 21};
-            default -> throw new LiamWetFishException("HUH!?");
+            default -> throw new IllegalStateException("Invalid view mode "
+                    + viewMode);
         };
     }
 
