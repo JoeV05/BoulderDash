9e1e688cb82bf99fd99e110f4f5648a4b15a1996
JoeV05 <josephvinson183@gmail.com>
2024-12-08 22:26:48 +0000

    Modified to fit CS-230 coding conventions

--- src/Tile.java
+++ src/Tile.java
@@ -1,28 +1,35 @@
 import javafx.scene.image.Image;
 
-// TODO - reconsider whether this class needs to exist under an entity hierarchy
-
 /**
+ * Represents a generic tile in the game. Tiles come in different types,
+ * each having unique properties related to behaviour.
  * @author Joseph Vinson
- * Represents a generic tile in the game
- * Tiles come in different types specified by enum.
- * Each having unique properties related to behaviour
- *
+ * @version 1.2
  */
-
-public abstract class Tile extends Entity{
+public abstract class Tile extends Entity {
     protected boolean walkable; //Indicates whether the tile can be walked on
-
     protected final TileType tileType; //Indicates what type of tile it is
 
-    public Tile(int x, int y, boolean walkable, TileType tileType, Image image) {
-        super(x, y, image); //calls the Entity constructor to set the tiles coordinates
+    /**
+     * Create a tile at the given (x, y) coordinates, of the given tile type
+     * and with a given sprite
+     * @param x The x coordinate for the tile.
+     * @param y The y coordinate for the tile.
+     * @param walkable Whether the tile can be walked on.
+     * @param type The type of the tile.
+     * @param sprite The sprite that should be used for the tile.
+     */
+    public Tile(int x, int y, boolean walkable, TileType type, Image sprite) {
+        super(x, y, sprite);
         this.walkable = walkable;
-        this.tileType = tileType;
+        this.tileType = type;
     }
 
-    // TODO - javadoc method comment
+    /**
+     * Check if this tile is walkable.
+     * @return true or false.
+     */
     public boolean isWalkable() {
-        return walkable; //Returns true if the tile is walkable, returns false if not
+        return walkable;
     }
 }
