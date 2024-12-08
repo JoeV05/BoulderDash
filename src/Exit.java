5ef82ff881755a6400e131eb4d1af2633641877b
JoeV05 <josephvinson183@gmail.com>
2024-12-08 19:54:49 +0000

    Modified to fit CS-230 coding conventions

--- src/Exit.java
+++ src/Exit.java
@@ -1,44 +1,59 @@
 import javafx.scene.image.Image;
 
-// TODO - proper class comment
-
 /**
- * @author Joseph Vinson
+ * Represents an exit to a specific level within the game.
+ * @author James Harvey, Joseph Vinson
  */
-
 public class Exit extends ActionWall {
     private static int scoreRequirement;
 
+    /**
+     * Creates an exit at the given (x, y) coordinates with the given score
+     * requirement.
+     * @param x The x coordinate of the exit.
+     * @param y The y coordinate of the exit.
+     * @param scoreReq The score requirement to use the exit.
+     */
     public Exit(int x, int y, int scoreReq) {
         super(x, y, WallType.EXIT);
         scoreRequirement = scoreReq;
     }
 
+    /**
+     * Set the required score for using the exit.
+     * @param scoreRequirement What the required score should be for using the
+     * exit.
+     */
     public static void setScoreRequirement(int scoreRequirement) {
         Exit.scoreRequirement = scoreRequirement;
     }
 
-    // TODO - javadoc method comment
+    /**
+     * Check if a number of diamonds is enough to use the exit.
+     * @param diamonds Number of diamonds to check against score requirement.
+     * @return true or false.
+     */
     public boolean canExit(int diamonds) {
         return diamonds >= scoreRequirement;
     }
 
-    // TODO - javadoc method comment
+    /**
+     * Unlocks the exit.
+     */
     private void unlock() {
         Image sprite = new Image("./sprites/exit_unlocked.png");
         this.setSprite(sprite);
         this.walkable = true;
     }
 
-    // TODO - javadoc method comment
-    public void updateExit(int diamonds) {
-        if (this.canExit(diamonds)) {
+    /**
+     * Check if the player has enough diamonds to leave the level, if so then
+     * unlock the exit.
+     */
+    @Override
+    public void tick() {
+        if (this.canExit(Player.getPlayer().getDiamonds())) {
             this.unlock();
         }
-    }
-
-    @Override
-    public void tick(){
-        updateExit(Player.getPlayer().getDiamonds());
     }
 }
