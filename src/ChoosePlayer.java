9e29ca14bf4df6968b049aef4cf6acec8c5056b7
JoeV05 <josephvinson183@gmail.com>
2024-12-08 19:54:04 +0000

    Modified to fit CS-230 coding conventions

--- src/ChoosePlayer.java
+++ src/ChoosePlayer.java
@@ -1,39 +1,71 @@
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+// TODO - whoever wrote this with no comments, i will find you and throw a fish at you
+
+/**
+ *
+ */
 public class ChoosePlayer {
     private static final String PF = "profiles.txt";
     private List<PlayerProfile> profiles;
     private PlayerProfile curentProfile;
 
+    /**
+     *
+     */
     public ChoosePlayer() {
         this.profiles = new ArrayList<>();
     }
 
+    /**
+     *
+     * @return
+     */
     public List<PlayerProfile> getProfiles() {
         return profiles;
     }
 
+    /**
+     *
+     * @param profiles
+     */
     public void setProfiles(List<PlayerProfile> profiles) {
         this.profiles = profiles;
     }
 
+    /**
+     *
+     * @return
+     */
     public PlayerProfile getCurentProfile() {
         return curentProfile;
     }
 
+    /**
+     *
+     * @param curentProfile
+     */
     public void setCurentProfile(PlayerProfile curentProfile) {
         this.curentProfile = curentProfile;
     }
 
+    /**
+     *
+     * @param name
+     */
     public void createProfile (String name) {
         PlayerProfile profile = new PlayerProfile(name);
         this.profiles.add(profile);
     }
+
+    /**
+     *
+     * @throws IOException
+     */
     private void saveProfile() throws IOException {
         try (PrintWriter writer = new PrintWriter(new FileWriter(PF))) {
             for (PlayerProfile profile : profiles) {
