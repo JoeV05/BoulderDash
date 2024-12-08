75284560742c4d0937a50f1f3687151441efaadb
Brucee-ee <153196545+Brucee-ee@users.noreply.github.com>
2024-12-08 02:48:48 +0000

    Added Basics

--- src/PlayerProfile.java
+++ src/PlayerProfile.java
@@ -1,2 +1,16 @@
 public class PlayerProfile {
+    private String name;
+    private int maxLevelUnlocked;
+
+    public PlayerProfile(String name, int maxLevelUnlocked) {
+        this.name = name;
+        this.maxLevelUnlocked = maxLevelUnlocked = 1; //Thats the default
+    }
+    public String getName() {
+        return name;
+    }
+    public int getMaxLevelUnlocked() {
+        return maxLevelUnlocked;
+    }
+
 }
