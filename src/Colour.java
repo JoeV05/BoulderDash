47bb33f93b1b8b7b5e3ede1cab8e8f000cfa3c1c
JoeV05 <josephvinson183@gmail.com>
2024-12-08 19:54:31 +0000

    Removed unused value from enumeration

--- src/Colour.java
+++ src/Colour.java
@@ -7,6 +7,5 @@ public enum Colour {
     RED, //Represents a red door/key
     GREEN, //Represents a green door/key
     YELLOW, //Represents a yellow door/key
-    BLUE, //Represents a blue door/key
-    NONE //Represents a door that has been broken open by a boulder
+    BLUE //Represents a blue door/key
 }
