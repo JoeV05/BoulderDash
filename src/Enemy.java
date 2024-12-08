5176f192b428ba50288eb302d4ab3d8b8df969f4
JoeV05 <josephvinson183@gmail.com>
2024-12-08 22:24:53 +0000

    Modified to fit CS-230 coding conventions

--- src/Enemy.java
+++ src/Enemy.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import javafx.scene.image.Image;
 
 /**
- * The Enemy Class which all enemies inherit from
+ * The Enemy Class which all enemies inherit from.
  * @version 1.0
  * @author Edward Tickle
  */
@@ -18,26 +18,24 @@ public abstract class Enemy extends Entity {
     }
 
     /**
-     * Moves the enemy
+     * Moves the enemy.
      */
     public abstract void move();
+
     /**
-     * A Test method designed to be used inside moveTo to test your movement is working correctly
+     * A Test method designed to be used inside moveTo to test your movement
+     * is working correctly.
      */
     @Test
     public abstract void movementTests();
+
     /**
-     * Performs any actions done when an enemy dies by a hazard and returns what they should drop on their death
-     * @return int representing a particular item or set of items to be dropped on enemy death
+     * Performs any actions done when an enemy dies by a hazard and returns
+     * what they should drop on their death.
      */
     public abstract void onDeathByHazard(Entity below);
 
     /**
-     * Performs any actions done when an enemy dies by a hazard and returns what they should drop on their death
-     * @return int representing a particular item or set of items to be dropped on enemy death
-     * Performs any actions done when an enemy dies by a falling object and executes any on death actions
-     * they should do on their death.
-     * @return nothing as it executes the actions needed itself
      * Performs any actions done when an enemy dies by a falling object and
      * executes any on death actions they should do on their death.
      * @param below Entity below the enemy.
