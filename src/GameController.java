c09b914838b97f2214a5b19adad8ef10577d83c8
JoeV05 <josephvinson183@gmail.com>
2024-12-08 11:08:38 +0000

    Removed unused code

--- src/GameController.java
+++ src/GameController.java
@@ -14,14 +14,6 @@ public class GameController {
     private static final int CANVAS_WIDTH = 750;
     private static final int CANVAS_HEIGHT = 400;
 
-    // The width and height (in pixels) of each cell that makes up the game.
-    private static final int CELL_WIDTH = 32;
-    private static final int CELL_HEIGHT = 32;
-
-    // The width of the grid in number of cells.
-    private static final int GRID_WIDTH = 40;
-    private static final int GRID_HEIGHT = 23;
-
     @FXML
     private static Canvas canvas;
     @FXML
@@ -34,21 +26,6 @@ public class GameController {
     private ImageView topRightImage2;
     @FXML
     private ImageView topRightImage3;
-
-
-    // TODO - javadoc method comment
-    // TODO - maybe not needed anymore?
-    //@FXML
-    // arrays work y then x.
-    public static void drawGame(Entity[][] levelState) {
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        for (int x = 0; x < GRID_WIDTH; x++) {
-            for (int y = 0; y < GRID_HEIGHT; y++) {
-                Image currentImage = levelState[y][x].getSprite();
-                gc.drawImage(currentImage, x * CELL_WIDTH, y * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT);
-            }
-        }
-    }
 
     private void setImage(ImageView image, boolean isYellow, int number) {
         String path = String.format("/%s/%d.png", isYellow ? "yellow" : "white", number);
